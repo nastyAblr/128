3b80e1687d10a475df6f1187235f41a18d7515be
Богачева Анастасия <britta18britta@gmail.com>
2024-12-15 20:10:42 +0300

    Add files via upload

---
+++ 128.py
@@ -0,0 +1,13 @@
+my_string = input('опишите Ваш вопрос: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string [0:1])
+print(my_string [-1])
+
+
+
+
+
+
